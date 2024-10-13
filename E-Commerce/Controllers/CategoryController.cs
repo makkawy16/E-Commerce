8b4a0e81f738e67b9fe610ab59b7b82f979248bd
Ahmed Mohsen <ahmedmohsen119981@outlook.com>
2024-10-13 10:51:11 +0300

    CategoryCRUD-Create

--- E-Commerce/Controllers/CategoryController.cs
+++ E-Commerce/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using E_Commerce.Data;
+using E_Commerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.Controllers
@@ -16,6 +17,23 @@ namespace E_Commerce.Controllers
         {
             var Categories = _context.categories.ToList();
             return View(Categories);
+        }
+        [HttpGet]
+        //this action is in form of get because it generates new page
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        //we need to get info from user
+        //Controller is the link between model and the view
+        public IActionResult Create(Category category)
+        {
+            _context.categories.Add(category);
+            _context.SaveChanges(); 
+
+            return RedirectToAction("Index");
         }
     }
 }
