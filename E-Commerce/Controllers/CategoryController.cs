2187310d9c9d625fce0a9e96f8572786ee98c5b9
Ahmed Mohsen <ahmedmohsen119981@outlook.com>
2024-10-13 10:10:22 +0300

    CategoryCRUD - Index

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        public IActionResult Index()
+        {
+            var Categories = _context.categories.ToList();
+            return View(Categories);
+        }
+    }
+}
