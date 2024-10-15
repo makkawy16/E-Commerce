d98c324a0aeab7aa9c039c097ae3af9894cc288a
Ahmed Mohsen <163747371+amohsen98@users.noreply.github.com>
2024-10-15 18:53:56 +0300

    Edit Products

--- E-Commerce/Areas/Admin/Controllers/ProductController.cs
+++ E-Commerce/Areas/Admin/Controllers/ProductController.cs
@@ -98,11 +98,65 @@ namespace E_Commerce.Areas.Admin.Controllers
             return View(productVM);
 
         }
+        [HttpGet]
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null | Id == 0)
+            {
+                NotFound();
+            }
+            ProductVM productVM = new ProductVM()
+            {
+                Product = _context.products.Find(Id),
+                //focus on this line might be problem
+                CategoryList = _context.categories.Select(x => new SelectListItem
+                {
 
-        public IActionResult Edit()
-        { return View(); }  
+                    Text = x.Name,
+                    Value = x.Id.ToString(),
+                }).ToList()
 
-        public IActionResult Delete() { return View(); }    
+            }; 
+            return View(productVM); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProductVM productVM, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string RootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString();
+                    var Upload = Path.Combine(RootPath, @"Images\Products");
+                    var ext = Path.GetExtension(file.FileName);
+                    if(productVM.Product.Img!= null)
+                    {
+
+                        var oldimg = Path.Combine(RootPath, productVM.Product.Img.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldimg)) { 
+                        
+                            System.IO.File.Delete(oldimg);
+                        }
+                    }   
+                    using (var filestream = new FileStream(Path.Combine(Upload, filename + ext), FileMode.Create))
+                    {
+                        file.CopyTo(filestream);
+                    }
+                    productVM.Product.Img = @"Images\Products\" + filename + ext;
+                }
+
+
+                _context.products.Update(productVM.Product);
+                _context.SaveChanges();
+                TempData["Update"] = "Item Has Been Created Succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(productVM.Product);
+
+        }
 
 
     }
