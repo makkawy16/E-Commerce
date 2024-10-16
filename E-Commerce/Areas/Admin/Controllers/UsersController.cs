55dcdefbaf720da860642d250b06ad98b395768d
Ahmed Mohsen <163747371+amohsen98@users.noreply.github.com>
2024-10-16 13:09:47 +0300

    Show Registered Users By Admin

---
+++ E-Commerce/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            string userid = claim.Value;
+
+            return View(_context.ApplicationUsers.Where(x=>x.Id !=userid).ToList());
+        }
+    }
+}
